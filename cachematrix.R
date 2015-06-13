508a6bb4e7d43a085c71fa4207ca7543d0ba2644
Jim Pfleger <jpfleger@acm.org>
2015-06-13 11:20:37 -0700

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a library for creating and using matrices whose inverse can be cached
+## Calculating a matrix inverse is a relatively expensive operation so we cache
+## the inverse once it's been calculated.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix(x) accepts a matrix x and returns an object that can hold
+## both the matrix and its inverse
+
+## Usage:
+## cm<-makeCacheMatrix(m)
+## cm$get() returns m
+## cm$getinverse() returns the cached inverse of m; returns NULL if there is
+##    no cached inverse
+## cm$set(m) caches a new matrix m, and clears the cached inverse
+## cm$setInv(newInv) caches newInv as the inverse of m
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -18,7 +29,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x) accepts an object x that was produced by makeCacheMatrix()
+## If x already has a cached inverse, that is returned
+## Otherwise the inverse is calculated, cached to x, and returned
+
+## Note that x is assumed to be invertible
 
 cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
